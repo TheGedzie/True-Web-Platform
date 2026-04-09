f730be9ff09902fdb217b65a3463df329f697c47
TheGedzie <gedzie06@gmail.com>
2026-04-09 09:23:16 +0700

    New page previewCoursePage + fix layout

---
+++ src/pages/PreviewCourse/PreviewCourse.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react'
+import cls from './previewCourse.module.css'
+import { Title } from '../../components/UI/Title'
+import { Button } from '../../components/UI/Button'
+import { useParams } from 'react-router-dom'
+import { getData } from '../../services/api'
+import arrow from '../../assets/arrow.svg'
+interface Course {
+  id: number
+  name: string
+  difficult: string
+  pages: number
+}
+
+export const PreviewCourse = () => {
+    const [course, setCourse] = useState<Course[] | null>(null);
+    const { id } = useParams();
+      useEffect(() => {
+          getData(`http://localhost:3000/courses/${id}`)
+              .then(response => {
+                  setCourse(response.data) 
+              })
+              .catch(error => console.error(error))
+      }, [])
+  return (
+    <div className={cls.PreviewCourse}>
+        <div className={cls.hero}>
+            <Title children={course?.name} color='green' size='large' level='h1'/>
+            <Button children='Начать' animation size='large'/>
+            <img src={arrow} alt="arrow" className={cls.arrow}/>
+        </div>
+        <div className={cls.description}>
+            <Title level='h2' size='medium' color='white'>{course?.name}</Title>
+            <div className={cls.testVideo}>
+                <span>video</span>
+            </div>
+            <p className={cls.descriptionText}>{course?.description}</p>
+        </div>
+       <div className={cls.intoCourse}>
+        <Title color='white'>Что входит в курс</Title>
+        <hr />
+        {course?.intoCourse.map(contentInto => (  
+            <p key={contentInto}>{contentInto}</p>
+        ))}
+        </div>
+    </div>
+  )
+}
---
+++ src/pages/PreviewCourse/index.ts
@@ -0,0 +1,1 @@
+export { PreviewCourse } from './previewCourse'
